eb240bc21aee87b22862beb5e454975390dd84e2
finish <bjj4016@gmai.com>
2017-06-21 10:02:57 +0900

    testz

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $ADMIN_INFO = NULL;
 $DEFAULT_KEYBOARD = NULL;
 // Update your base url.
-define("BASE_URL","");
+define("BASE_URL","https://phptestfirst.herokuapp.com/");
 // Only access kakao server or not.
 define("IP_CHECK", TRUE);
 // Show all warnings or not.
